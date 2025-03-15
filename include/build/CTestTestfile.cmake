2d88bb311f02db235596c9cf0e36aae8a98cfb9d
bartek1009x <bartek1009x@gmail.com>
2025-03-15 13:20:04 +0100

    Cleanup

--- include/build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/barte/Desktop/VoltaRewritten/include
-# Build directory: C:/Users/barte/Desktop/VoltaRewritten/include/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(lua-testsuite "C:/Users/barte/Desktop/VoltaRewritten/include/build/lua.exe" "-e" "_U=true" "all.lua")
-set_tests_properties(lua-testsuite PROPERTIES  WORKING_DIRECTORY "C:/Users/barte/Desktop/VoltaRewritten/include/lua-5.4.7-tests" _BACKTRACE_TRIPLES "C:/Users/barte/Desktop/VoltaRewritten/include/CMakeLists.txt;31;add_test;C:/Users/barte/Desktop/VoltaRewritten/include/CMakeLists.txt;0;")
-subdirs("lua-5.4.7")
--- include/glfw-3.4/build/tests/CMakeFiles/inputlag.dir/__/deps/getopt.c.obj.d
+++
@@ -1,8 +0,0 @@
-tests/CMakeFiles/inputlag.dir/__/deps/getopt.c.obj: \
- C:\Users\barte\Desktop\VoltaRewritten\include\glfw-3.4\deps\getopt.c \
- C:\Users\barte\Desktop\VoltaRewritten\include\glfw-3.4\deps\getopt.h \
- c:\mingw\lib\gcc\mingw32\6.3.0\include\stddef.h \
- c:\mingw\include\string.h c:\mingw\include\_mingw.h \
- c:\mingw\include\msvcrtver.h c:\mingw\include\w32api.h \
- c:\mingw\include\sdkddkver.h c:\mingw\include\strings.h \
- c:\mingw\include\wchar.h
