8b8d31d961acb9bf21f8e7df7807dcc5e46ac6e9
bartek1009x <bartek1009x@gmail.com>
2025-03-30 18:26:50 +0200

    VoltaNet added

---
+++ include/voltanet-1.0.0/build/CMakeFiles/VoltaNet.dir/DependInfo.cmake
@@ -0,0 +1,43 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/barte/Desktop/VoltaFramework/include/voltanet-1.0.0/VoltaNet.cpp" "CMakeFiles/VoltaNet.dir/VoltaNet.cpp.obj" "gcc" "CMakeFiles/VoltaNet.dir/VoltaNet.cpp.obj.d"
+  "C:/Users/barte/Desktop/VoltaFramework/include/voltanet-1.0.0/cryptopp8.9/algebra.cpp" "CMakeFiles/VoltaNet.dir/cryptopp8.9/algebra.cpp.obj" "gcc" "CMakeFiles/VoltaNet.dir/cryptopp8.9/algebra.cpp.obj.d"
+  "C:/Users/barte/Desktop/VoltaFramework/include/voltanet-1.0.0/cryptopp8.9/allocate.cpp" "CMakeFiles/VoltaNet.dir/cryptopp8.9/allocate.cpp.obj" "gcc" "CMakeFiles/VoltaNet.dir/cryptopp8.9/allocate.cpp.obj.d"
+  "C:/Users/barte/Desktop/VoltaFramework/include/voltanet-1.0.0/cryptopp8.9/asn.cpp" "CMakeFiles/VoltaNet.dir/cryptopp8.9/asn.cpp.obj" "gcc" "CMakeFiles/VoltaNet.dir/cryptopp8.9/asn.cpp.obj.d"
+  "C:/Users/barte/Desktop/VoltaFramework/include/voltanet-1.0.0/cryptopp8.9/basecode.cpp" "CMakeFiles/VoltaNet.dir/cryptopp8.9/basecode.cpp.obj" "gcc" "CMakeFiles/VoltaNet.dir/cryptopp8.9/basecode.cpp.obj.d"
+  "C:/Users/barte/Desktop/VoltaFramework/include/voltanet-1.0.0/cryptopp8.9/cryptlib.cpp" "CMakeFiles/VoltaNet.dir/cryptopp8.9/cryptlib.cpp.obj" "gcc" "CMakeFiles/VoltaNet.dir/cryptopp8.9/cryptlib.cpp.obj.d"
+  "C:/Users/barte/Desktop/VoltaFramework/include/voltanet-1.0.0/cryptopp8.9/eccrypto.cpp" "CMakeFiles/VoltaNet.dir/cryptopp8.9/eccrypto.cpp.obj" "gcc" "CMakeFiles/VoltaNet.dir/cryptopp8.9/eccrypto.cpp.obj.d"
+  "C:/Users/barte/Desktop/VoltaFramework/include/voltanet-1.0.0/cryptopp8.9/ecp.cpp" "CMakeFiles/VoltaNet.dir/cryptopp8.9/ecp.cpp.obj" "gcc" "CMakeFiles/VoltaNet.dir/cryptopp8.9/ecp.cpp.obj.d"
+  "C:/Users/barte/Desktop/VoltaFramework/include/voltanet-1.0.0/cryptopp8.9/filters.cpp" "CMakeFiles/VoltaNet.dir/cryptopp8.9/filters.cpp.obj" "gcc" "CMakeFiles/VoltaNet.dir/cryptopp8.9/filters.cpp.obj.d"
+  "C:/Users/barte/Desktop/VoltaFramework/include/voltanet-1.0.0/cryptopp8.9/gcm.cpp" "CMakeFiles/VoltaNet.dir/cryptopp8.9/gcm.cpp.obj" "gcc" "CMakeFiles/VoltaNet.dir/cryptopp8.9/gcm.cpp.obj.d"
+  "C:/Users/barte/Desktop/VoltaFramework/include/voltanet-1.0.0/cryptopp8.9/gcm_simd.cpp" "CMakeFiles/VoltaNet.dir/cryptopp8.9/gcm_simd.cpp.obj" "gcc" "CMakeFiles/VoltaNet.dir/cryptopp8.9/gcm_simd.cpp.obj.d"
+  "C:/Users/barte/Desktop/VoltaFramework/include/voltanet-1.0.0/cryptopp8.9/hex.cpp" "CMakeFiles/VoltaNet.dir/cryptopp8.9/hex.cpp.obj" "gcc" "CMakeFiles/VoltaNet.dir/cryptopp8.9/hex.cpp.obj.d"
+  "C:/Users/barte/Desktop/VoltaFramework/include/voltanet-1.0.0/cryptopp8.9/integer.cpp" "CMakeFiles/VoltaNet.dir/cryptopp8.9/integer.cpp.obj" "gcc" "CMakeFiles/VoltaNet.dir/cryptopp8.9/integer.cpp.obj.d"
+  "C:/Users/barte/Desktop/VoltaFramework/include/voltanet-1.0.0/cryptopp8.9/misc.cpp" "CMakeFiles/VoltaNet.dir/cryptopp8.9/misc.cpp.obj" "gcc" "CMakeFiles/VoltaNet.dir/cryptopp8.9/misc.cpp.obj.d"
+  "C:/Users/barte/Desktop/VoltaFramework/include/voltanet-1.0.0/cryptopp8.9/nbtheory.cpp" "CMakeFiles/VoltaNet.dir/cryptopp8.9/nbtheory.cpp.obj" "gcc" "CMakeFiles/VoltaNet.dir/cryptopp8.9/nbtheory.cpp.obj.d"
+  "C:/Users/barte/Desktop/VoltaFramework/include/voltanet-1.0.0/cryptopp8.9/osrng.cpp" "CMakeFiles/VoltaNet.dir/cryptopp8.9/osrng.cpp.obj" "gcc" "CMakeFiles/VoltaNet.dir/cryptopp8.9/osrng.cpp.obj.d"
+  "C:/Users/barte/Desktop/VoltaFramework/include/voltanet-1.0.0/cryptopp8.9/queue.cpp" "CMakeFiles/VoltaNet.dir/cryptopp8.9/queue.cpp.obj" "gcc" "CMakeFiles/VoltaNet.dir/cryptopp8.9/queue.cpp.obj.d"
+  "C:/Users/barte/Desktop/VoltaFramework/include/voltanet-1.0.0/cryptopp8.9/rijndael.cpp" "CMakeFiles/VoltaNet.dir/cryptopp8.9/rijndael.cpp.obj" "gcc" "CMakeFiles/VoltaNet.dir/cryptopp8.9/rijndael.cpp.obj.d"
+  "C:/Users/barte/Desktop/VoltaFramework/include/voltanet-1.0.0/cryptopp8.9/rijndael_simd.cpp" "CMakeFiles/VoltaNet.dir/cryptopp8.9/rijndael_simd.cpp.obj" "gcc" "CMakeFiles/VoltaNet.dir/cryptopp8.9/rijndael_simd.cpp.obj.d"
+  "C:/Users/barte/Desktop/VoltaFramework/include/voltanet-1.0.0/cryptopp8.9/sha.cpp" "CMakeFiles/VoltaNet.dir/cryptopp8.9/sha.cpp.obj" "gcc" "CMakeFiles/VoltaNet.dir/cryptopp8.9/sha.cpp.obj.d"
+  "C:/Users/barte/Desktop/VoltaFramework/include/voltanet-1.0.0/cryptopp8.9/sha_simd.cpp" "CMakeFiles/VoltaNet.dir/cryptopp8.9/sha_simd.cpp.obj" "gcc" "CMakeFiles/VoltaNet.dir/cryptopp8.9/sha_simd.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
